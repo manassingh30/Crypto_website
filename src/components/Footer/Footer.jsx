56896d09f65c526d7acafd583ef61cdfbd0706b8
Manas Singh <106422703+manassingh30@users.noreply.github.com>
2025-02-18 12:10:49 +0530

    Final commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Copyright @Cryptoplace - All Rights Reserved. 
+      By `Manas`.</p>
+    </div>
+  )
+}
+
+export default Footer
